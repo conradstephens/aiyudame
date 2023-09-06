419750e7d932a3c0c1159465b631732a4f26c4f2
conradstephens <conradastephens@gmail.com>
2023-09-05 21:07:31 -0400

    updated prisma import

--- src/app/api/deleteHistory/route.ts
+++ src/app/api/deleteHistory/route.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import prisma from "@/lib/prisma";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
